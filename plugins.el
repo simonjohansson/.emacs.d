2916c90191def408ab08c9ff00c4432cc88a961f
Simon Johansson <simon@simonjohansson.com>
2014-08-27 14:57:22 +0200

    Change keybindings for mark multiple.
    Acked-by: Simon Johansson <simon@simonjohansson.com>
    Acked-by: Simon Johansson <simon@simonjohansson.com>
    Acked-by: Simon Johansson <simon@simonjohansson.com>

--- plugins.el
+++ plugins.el
@@ -3,6 +3,6 @@
 (define-key global-map (kbd "M-SPC") 'ace-jump-mode)
 
 (require 'multiple-cursors)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c C-n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-p") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-a") 'mc/mark-all-like-this)
