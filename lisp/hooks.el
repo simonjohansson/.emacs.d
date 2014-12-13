f201b36b762037bafe4e6ca45aaf9dfef60b453b
Simon Johansson <simon@Simons-MacBook-Pro.local>
2014-12-13 11:29:51 +0100

    Indent correctly.

--- lisp/hooks.el
+++ lisp/hooks.el
@@ -6,11 +6,11 @@
   (add-hook a-mode 'rainbow-delimiters-mode)
   (add-hook a-mode 'enable-paredit-mode)
   (add-hook a-mode '(lambda ()
-		      (local-set-key (kbd "RET") 'newline-and-indent))))
+											(local-set-key (kbd "RET") 'newline-and-indent))))
 
 ;; YAML-mode
 (add-hook 'yaml-mode-hook (lambda ()
-			    (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+														(define-key yaml-mode-map "\C-m" 'newline-and-indent)))
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
 
