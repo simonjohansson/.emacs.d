5ccef9414be515a39e516fc18de719ef7194ab6f
Simon Johansson <simon@Simons-MacBook-Pro.local>
2014-12-13 11:36:34 +0100

    Setup my path.

--- lisp/env.el
+++ lisp/env.el
@@ -1,5 +1,7 @@
-;; Add my ~/bin to the path
-(setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/bin")))
-(setq exec-path (append exec-path '(expand-file-name "~/bin")))
+;; Setup my PATH.
+(setenv "PATH" (concat
+		(getenv "PATH") ":"
+		(expand-file-name "~/bin") ":"
+		"/usr/local/bin"))
 
 (setq default-directory (expand-file-name "~/"))
