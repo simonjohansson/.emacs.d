6015c4050bc7b5008db8a48f70fc17c3a58756bb
Simon Johansson <simon@Simons-MacBook-Pro.local>
2014-12-13 11:29:07 +0100

    Remove unused settings in env.

--- lisp/env.el
+++ lisp/env.el
@@ -2,26 +2,4 @@
 (setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/bin")))
 (setq exec-path (append exec-path '(expand-file-name "~/bin")))
 
-;; Set default-directory to ~/ instead of /. Since Im using systemd
-;; to start the emacs daemon.
-(setq default-directory "~/")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" "dd4db38519d2ad7eb9e2f30bc03fba61a7af49a185edfd44e020aa5345e3dca7" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(send-mail-function (quote sendmail-send-it)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (setq default-directory (expand-file-name "~/"))
-
-;; 4 spaces per tab plz.
-(setq-default tab-width 4)
