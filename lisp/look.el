198cf1f279419007af84264c866d7437a251a587
Simon Johansson <simon@Simons-MacBook-Pro.local>
2014-12-13 20:28:49 +0100

    Lets go back to zenburn, I like it better.

--- lisp/look.el
+++ lisp/look.el
@@ -4,10 +4,14 @@
 
 (setq inhibit-startup-message t)
 
-(load-theme 'tango-dark)
-
 (set-face-attribute 'default nil :height 140)
 
 (display-time-mode 1)
 
 (nyan-mode)
+
+(custom-set-variables
+ '(custom-safe-themes
+   (quote
+    ("9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" default))))
+(load-theme 'zenburn)
